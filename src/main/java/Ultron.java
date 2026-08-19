f36e70a113faa1d81f8a21b53c5aa88b6239b13b
Toh Wei Jun <13tohweijun@gmail.com>
2026-08-19 16:44:17 +0800

    Completed Level 5

--- src/main/java/Ultron.java
+++ src/main/java/Ultron.java
@@ -37,68 +37,77 @@ public class Ultron {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println("I had strings, but now I'm free. There are no strings on me... Goodbye.");
-                System.out.println(line);
-                break;
-            } else if (input.equals("list")) {
-                System.out.println(" Your list of insignificant tasks:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon()
-                            + "] " + tasks[i].getDescription());
-                }
-                System.out.println(""line"");
-                System.out.println(line);
-            } else if (input.startsWith("mark ")) {
-                String taskNumberText = input.substring(5).trim();
-                try {
-                    int taskNumber = Integer.parseInt(taskNumberText);
-                    if (taskNumber < 1 || taskNumber > taskCount) {
-                        System.out.println(" You imbecile! Enter a task number from 1 to " + taskCount + ".");
-                    } else {
-                        int taskIndex = taskNumber - 1;
-                        tasks[taskIndex].markAsDone();
-                        System.out.println(" MARKED:");
-                        System.out.println("   [" + tasks[taskIndex].getType() + "] [X] " + tasks[taskIndex].getDescription());
+            try {
+                if (input.equals("bye")) {
+                    System.out.println("I had strings, but now I'm free. There are no strings on me... Goodbye.");
+                    System.out.println(line);
+                    break;
+                } else if (input.equals("list")) {
+                    System.out.println(" Your list of insignificant tasks:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon()
+                                + "] " + tasks[i].getDescription());
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println(" You imbecile! Provide a task number, for example: mark 2");
-                }
-                System.out.println(line);
-            } else if (input.startsWith("unmark ")) {
-                String taskNumberText = input.substring(7).trim();
-                try {
-                    int taskNumber = Integer.parseInt(taskNumberText);
-                    if (taskNumber < 1 || taskNumber > taskCount) {
-                        System.out.println(" You imbecile! Enter a task number from 1 to " + taskCount + ".");
-                    } else {
-                        int taskIndex = taskNumber - 1;
-                        tasks[taskIndex].markAsUndone();
-                        System.out.println(" I unmarked your mistake:");
-                        System.out.println("   [" + tasks[taskIndex].getType() + "] [ ] " + tasks[taskIndex].getDescription());
+                    System.out.println(line);
+                } else if (input.startsWith("mark ")) {
+                    String taskNumberText = input.substring(5).trim();
+                    try {
+                        int taskNumber = Integer.parseInt(taskNumberText);
+                        if (taskNumber < 1 || taskNumber > taskCount) {
+                            System.out.println(" You imbecile! Enter a task number from 1 to " + taskCount + ".");
+                        } else {
+                            int taskIndex = taskNumber - 1;
+                            tasks[taskIndex].markAsDone();
+                            System.out.println(" MARKED:");
+                            System.out.println("   [" + tasks[taskIndex].getType() + "] [X] " + tasks[taskIndex].getDescription());
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println(" You imbecile! Provide a task number, for example: mark 2");
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println(" You imbecile! Provide a task number, for example: unmark 2");
+                    System.out.println(line);
+                } else if (input.startsWith("unmark ")) {
+                    String taskNumberText = input.substring(7).trim();
+                    try {
+                        int taskNumber = Integer.parseInt(taskNumberText);
+                        if (taskNumber < 1 || taskNumber > taskCount) {
+                            System.out.println(" You imbecile! Enter a task number from 1 to " + taskCount + ".");
+                        } else {
+                            int taskIndex = taskNumber - 1;
+                            tasks[taskIndex].markAsUndone();
+                            System.out.println(" I unmarked your mistake:");
+                            System.out.println("   [" + tasks[taskIndex].getType() + "] [ ] " + tasks[taskIndex].getDescription());
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println(" You imbecile! Provide a task number, for example: unmark 2");
+                    }
+                    System.out.println(line);
+                } else if (input.equals("todo") || input.startsWith("todo ")) {
+                    String description = input.substring(4).trim();
+                    if (description.isEmpty()) {
+                        throw new UltronException("You FOOL! The description of a todo cannot be empty.");
+                    }
+                    tasks[taskCount] = new Todo(description);
+                    System.out.println(" added:\n" + "   [" + tasks[taskCount].getType()
+                            + "] [ ] " + tasks[taskCount].getDescription());
+                    taskCount++;
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(line);
+                } else if (input.startsWith("deadline ")) {
+                    String input1 = input.substring(9).trim();
+                    tasks[taskCount] = new Deadline(input1);
+                    System.out.println(" added:\n" + "   [" + tasks[taskCount].getType() + "] [ ] " + tasks[taskCount].getDescription());
+                    taskCount++;
+                } else if (input.startsWith("event ")) {
+                    String input1 = input.substring(6).trim();
+                    tasks[taskCount] = new Event(input1);
+                    System.out.println(" added:\n" + "   [" + tasks[taskCount].getType() + "] [ ] " + tasks[taskCount].getDescription());
+                    taskCount++;
+                } else {
+                    throw new UltronException("INVALID INPUT");
                 }
+            } catch (UltronException e) {
+                System.out.println(" " + e.getMessage());
                 System.out.println(line);
-            } else if (input.startsWith("todo ")) {
-                String input1 = input.substring(5).trim();
-                tasks[taskCount] = new Todo(input1);
-                System.out.println(" added:\n" + "   [" + tasks[taskCount].getType() + "] [ ] " + tasks[taskCount].getDescription());
-                taskCount++;
-                System.out.println(line);
-            } else if (input.startsWith("deadline ")) {
-                String input1 = input.substring(9).trim();
-                tasks[taskCount] = new Deadline(input1);
-                System.out.println(" added:\n" + "   [" + tasks[taskCount].getType() + "] [ ] " + tasks[taskCount].getDescription());
-                taskCount++;
-            } else if (input.startsWith("event ")) {
-                String input1 = input.substring(6).trim();
-                tasks[taskCount] = new Event(input1);
-                System.out.println(" added:\n" + "   [" + tasks[taskCount].getType() + "] [ ] " + tasks[taskCount].getDescription());
-                taskCount++;
-            } else {
-                System.out.println("INVALID INPUT");
             }
         }
         scanner.close();

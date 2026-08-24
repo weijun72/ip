b03f40c84c90ffd900bf9e9c94337edfd3b56ffe
Toh Wei Jun <13tohweijun@gmail.com>
2026-08-24 17:56:17 +0800

    new test for gradle

---
+++ src/test/java/ultron/model/DeadlineTest.java
@@ -0,0 +1,38 @@
+package ultron.model;
+
+import org.junit.jupiter.api.Test;
+
+import ultron.exception.UltronException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Tests deadline parsing and display formatting.
+ */
+class DeadlineTest {
+
+    @Test
+    void getDescription_deadlineWithDate_formatsDate() throws UltronException {
+        Deadline deadline = new Deadline("return book /by 2/12/2019");
+
+        assertEquals("return book( by: 02/Dec/2019 )", deadline.getDescription());
+    }
+
+    @Test
+    void getDescription_deadlineWithDateAndTime_formatsDateAndTime() throws UltronException {
+        Deadline deadline = new Deadline("return book /by 2/12/2019 1800");
+
+        assertEquals("return book( by: 02/Dec/2019 1800 )", deadline.getDescription());
+    }
+
+    @Test
+    void constructor_missingByMarker_exceptionThrown() {
+        assertThrows(UltronException.class, () -> new Deadline("return book"));
+    }
+
+    @Test
+    void constructor_invalidDate_exceptionThrown() {
+        assertThrows(UltronException.class, () -> new Deadline("return book /by tomorrow"));
+    }
+}
